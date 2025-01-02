bb714dbb45750b2c44e76ffcbb71b88f4ca9ca6b
Natanael-hash <natanaelhordon@yahoo.com>
2025-01-02 17:45:57 +0200

    add more images for detection and modify the list of classes

--- data_preparation/extract_object_info_from_xml.py
+++ data_preparation/extract_object_info_from_xml.py
@@ -121,7 +121,7 @@ def label_encoding(label):
         'person': 0, 'car': 1, 'chair': 2, 'bottle': 3, 'pottedplant': 4, 'bird': 5,
         'dog': 6, 'sofa': 7, 'bicycle': 8, 'horse': 9, 'boat': 10, 'motorbike': 11,
         'cat': 12, 'tvmonitor': 13, 'cow': 14, 'sheep': 15, 'aeroplane': 16,
-        'train': 17, 'diningtable': 18, 'bus': 19
+        'train': 17, 'diningtable': 18, 'bus': 19, 'cup':20
     }
     try:
         return labels[label]
