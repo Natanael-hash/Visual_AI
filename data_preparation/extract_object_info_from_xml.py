2dcac642780008504d7022dd1c13d15c29218a4d
Natanael-hash <natanaelhordon@yahoo.com>
2025-01-03 10:34:18 +0200

    add carrot to the classes and update yaml file with it.

--- data_preparation/extract_object_info_from_xml.py
+++ data_preparation/extract_object_info_from_xml.py
@@ -15,7 +15,7 @@ logging.basicConfig(
     ]
 )
 
-xml_list = glob("data_images/annotations/*.xml")
+xml_list = glob("data_images/*.xml")
 
 def extract_text(filename):
     """
@@ -121,7 +121,8 @@ def label_encoding(label):
         'person': 0, 'car': 1, 'chair': 2, 'bottle': 3, 'pottedplant': 4, 'bird': 5,
         'dog': 6, 'sofa': 7, 'bicycle': 8, 'horse': 9, 'boat': 10, 'motorbike': 11,
         'cat': 12, 'tvmonitor': 13, 'cow': 14, 'sheep': 15, 'aeroplane': 16,
-        'train': 17, 'diningtable': 18, 'bus': 19, 'cup':20, 'truck':21
+        'train': 17, 'diningtable': 18, 'bus': 19, 'cup':20, 'truck':21, 'carrot':22,
+        'potato':23
     }
     try:
         return labels[label]
