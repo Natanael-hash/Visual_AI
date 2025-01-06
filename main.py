fca0d430290881913d3b414d2451f9330def3ba7
Natanael-hash <natanaelhordon@yahoo.com>
2025-01-06 17:12:38 +0200

    Added best.pt model for detection in real-time using your phone camera

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from ultralytics import YOLO
 
-model = YOLO("image_processing/train/weights/best.pt")
+model = YOLO("best.pt")
 
-model.predict(source="image_processing/4K Road traffic video for object detection and tracking - free download now! [MNn9qKG2UFI].webm", save=True, imgsz=320, conf=0.5, device="mps", show=True)
+model.predict(0, save=True, imgsz=320, conf=0.5, device="mps", show=True)
