a4833135bdf46692fb53c98d4d627800983d2f4f
Natanael-hash <natanaelhordon@yahoo.com>
2025-01-06 17:23:30 +0200

    change image size parameter from imgsz=320 to imgsz=640

--- main.py
+++ main.py
@@ -2,4 +2,4 @@ from ultralytics import YOLO
 
 model = YOLO("best.pt")
 
-model.predict(0, save=True, imgsz=320, conf=0.5, device="mps", show=True)
+model.predict(0, save=True, imgsz=640, conf=0.5, device="mps", show=True)
