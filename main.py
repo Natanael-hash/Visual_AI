392bd4fa9ce165a291fa135d4e7472eac2e1badb
Natanael-hash <natanaelhordon@yahoo.com>
2025-03-11 23:29:34 +0200

    Modify inference

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from src.data_prediction.yolo_predictions import ObjectDetection
 
 
-detector = ObjectDetection(capture_index=0, data_yaml='/Users/natanaelhordon/Desktop/Visual-AI/src/data_preparation/data.yaml')
+detector = ObjectDetection(capture_index=0)
 detector()
--- src/data_prediction/yolo_predictions.py
+++ src/data_prediction/yolo_predictions.py
@@ -3,21 +3,15 @@ import numpy as np
 import cv2
 from time import time
 from ultralytics import YOLO
-import yaml
-from yaml.loader import SafeLoader
 
 class ObjectDetection:
-    def __init__(self, capture_index, data_yaml):
+    def __init__(self, capture_index):
         self.colors = None
         self.capture_index = capture_index
 
         self.device = 'mps' if torch.mps.is_available() else 'cpu'
         self.model = self.load_model()
 
-        with open(data_yaml, mode='r') as f:
-            data_yaml = yaml.load(f, Loader=SafeLoader)
-
-        self.nc = data_yaml['nc']
 
     def load_model(self):
         model = YOLO("/Users/natanaelhordon/Desktop/Visual-AI/src/data_prediction/train7/weights/best.pt")
@@ -35,21 +29,12 @@ class ObjectDetection:
 
         for result in results:
             boxes = result.boxes.cpu().numpy()
-            for box in boxes:
-                x1, y1, x2, y2 = map(int, box.xyxys[0])
-                confidence = box.conf[0]
-                class_id = int(box.cls[0])
-                self.colors = self.generate_colors(class_id)
+            
+            xyxys.append(boxes.xyxy)
+            confidences.append(boxes.conf)
+            class_ids.append(boxes.cls)
 
-                xyxys.append((x1, y1, x2, y2))
-                confidences.append(confidence)
-                class_ids.append(class_id)
-
-                cv2.rectangle(frame, (x1, y1), (x2, y2), self.colors, 2)
-                label = f'{self.model.names[class_id]} {confidence:.2f}'
-                cv2.putText(frame, label, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, self.colors, 2)
-
-        return frame, xyxys, confidences, class_ids
+        return results[0].plot(line_width=5, font_size=5), xyxys, confidences, class_ids
 
     def __call__(self):
         cap = cv2.VideoCapture(self.capture_index)
@@ -61,11 +46,11 @@ class ObjectDetection:
 
             start_time = time()
             results = self.predict(frame)
-            frame, _, _, _ = self.plot_bboxes(results, frame)
+            frame, _,_,_ = self.plot_bboxes(results, frame)
             end_time = time()
 
             fps = 1 / (end_time - start_time)
-            cv2.putText(frame, f'FPS: {fps:.2f}', (10, 90), cv2.FONT_HERSHEY_PLAIN, 1, (0, 255, 0), 5)
+            cv2.putText(frame, f'FPS: {fps:.2f}', (10, 90), cv2.FONT_HERSHEY_PLAIN, 3, (0, 255, 0), 2)
 
             cv2.imshow('YOLOv8 Detection', frame)
 
@@ -76,9 +61,5 @@ class ObjectDetection:
         cv2.destroyAllWindows()
 
 
-    def generate_colors(self, ID):
-        np.random.seed(10)
-        colors = np.random.randint(100, 255, size=(self.nc, 3)).tolist()
-        return tuple(colors[ID])
 
 
